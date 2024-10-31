7c4f102adfa31e9e1ca79f9c647d141907e4f1a7
hv.lopez <hv.lopez@uniandes.edu.co>
2024-10-30 20:24:13 -0600

    feat: add readme and update monkey test

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require("cypress");
-
+const merge = require('mochawesome-merge');
+const marge = require('mochawesome-report-generator');
 module.exports = defineConfig({
   e2e: {
     video: true,
@@ -12,7 +13,27 @@ module.exports = defineConfig({
       json: true,
     },
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('after:run', async (results) => {
+        try {
+          const jsonReport = await merge({
+            files: ['results/*.json'],
+          });
+
+          const fs = require('fs');
+          fs.writeFileSync('results/combined-report.json', JSON.stringify(jsonReport, null, 2));
+
+          console.log("Archivos JSON combinados exitosamente.");
+
+          await marge.create(jsonReport, {
+            reportDir: 'results',
+            saveJson: true,
+          });
+
+          console.log("Reporte HTML generado exitosamente en la carpeta results.");
+        } catch (error) {
+          console.error(`Error al generar el reporte: ${error.message}`);
+        }
+      });
     },
   },
 });
